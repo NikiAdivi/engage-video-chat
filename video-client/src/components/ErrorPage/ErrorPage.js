71a82bd6c687a6533e5730a837b6af28491a06eb
NikiAdivi <geenanik25@gmail.com>
2021-06-24 02:37:22 +0530

    Added ErrorPage UI

--- video-client/src/components/ErrorPage/ErrorPage.js
+++ video-client/src/components/ErrorPage/ErrorPage.js
@@ -1,9 +1,20 @@
-import './ErrorPage.scss'
+// import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
+import Header from "../UI/HeaderHomePage/HeaderHomePage";
 
 const ErrorPage = () => {
-    return (
-        <h1>ErrorPage</h1>
-    )
-}
-
+  return (
+    <div className="no-match">
+      <Header />
+      <div className="no-match__content">
+        <h2>Invalid video call name.</h2>
+        <div className="action-btn">
+          {/* <Link className="btn green" to="/"> */}
+            Return to home screen
+          {/* </Link> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ErrorPage;
